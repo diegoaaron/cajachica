fb514a79dbe7f7ebb6360d9930c6b47c8943c1a9
Diego Aaron <diegoaarondv@gmail.com>
2024-09-21 01:24:36 -0500

    inicio

---
+++ src/main/java/com/integrity/cajachica/CajachicaApplication.java
@@ -0,0 +1,13 @@
+package com.integrity.cajachica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajachicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajachicaApplication.class, args);
+	}
+
+}
