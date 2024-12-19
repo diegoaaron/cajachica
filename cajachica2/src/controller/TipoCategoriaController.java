d395b6cd0de1e6e3e2592382d2923e9ff775aa97
Diego <diegoaarondv@gmail.com>
2024-12-19 16:01:17 -0500

    actualizando controller 2

--- cajachica2/src/controller/TipoCategoriaController.java
+++ cajachica2/src/controller/TipoCategoriaController.java
@@ -1,49 +1,27 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
-import conexion.Conexion;
 import model.TipoCategoria;
+import model.TipoCategoriaModel;
 
 public class TipoCategoriaController {
 
-	private final Connection cnn4;
-	private ArrayList<TipoCategoria> tipoCategoriaArrayList;
+	public TipoCategoriaController() {
 
-	public TipoCategoriaController(Connection cnn) {
-		this.cnn4 = cnn;
 	}
 
-	public ArrayList<TipoCategoria> listaTipoCategoria() {
-		Statement st4 = null;
-		ResultSet rs4 = null;
-
-		TipoCategoria tipoCategoria;
-		tipoCategoriaArrayList = new ArrayList<>();
+	// método para listar los tipos de categorias de comprobantes
+	public ArrayList<TipoCategoria> listarTipoCategoria() {
+		TipoCategoriaModel tipoCategoriaModel = new TipoCategoriaModel();
+		ArrayList<TipoCategoria> listaCategorias = null;
 
 		try {
-			String sql = "select * from Categoria_Gasto";
-			st4 = cnn4.createStatement();
-			rs4 = st4.executeQuery(sql);
-
-			while (rs4.next()) {
-				int categoria_id = rs4.getInt("categoria_id");
-				String nombre_categoria = rs4.getString("nombre_categoria");
-				tipoCategoria = new TipoCategoria(categoria_id, nombre_categoria);
-				tipoCategoriaArrayList.add(tipoCategoria);
-			}
-
+			listaCategorias = tipoCategoriaModel.listarTipoCategoria();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			Conexion.cerrar(st4, rs4);
 		}
 
-		return tipoCategoriaArrayList;
-
+		return listaCategorias;
 	}
-
 }
